ef593024e3b48873a0548592d7456d4e6f88f474
TeddyBaloyi <teddybc63@email.com>
2026-05-09 01:21:44 +0200

    Loading data into the silver layer

--- Load and cleaning in the silver layer.sql
+++ Load and cleaning in the silver layer.sql
@@ -1,3 +1,51 @@
 ----Check for Nulls and duplicates in Primary keys
-----expectations: No results
-SELECT 
+----expectations: No result
+
+SELECT prd_key,COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_key
+HAVING COUNT(*) > 1 OR prd_key IS NULL
+
+SELECT*
+FROM bronze.crm_prd_info
+
+-----CHECK THE UNWANTED SPACES
+---- EXPECTATIONS: NO RESULTS
+ 
+ SELECT cst_firstname
+ FROM silver.crm_cst_info
+ WHERE cst_firstname != TRIM(cst_firstname)
+
+ SELECT cst_lastname
+ FROM silver.crm_cst_info
+ WHERE cst_lastname != TRIM(cst_lastname)
+
+ SELECT cst_firstname
+ FROM silver.crm_cst_info
+ WHERE cst_firstname != TRIM(cst_firstname)
+
+ SELECT cst_material_status
+ FROM silver.crm_cst_info
+ WHERE cst_material_status != TRIM(cst_material_status)
+
+
+ SELECT cst_gndr
+ FROM silver.crm_cst_info
+ WHERE cst_gndr != TRIM(cst_gndr)
+
+ ----Data standadisation and consistancy
+
+ SELECT DISTINCT prd_line
+ FROM bronze.crm_prd_info
+
+ SELECT 
+    COUNT(*) AS null_count
+FROM bronze.crm_prd_info
+WHERE prd_line IS NULL;
+
+ SELECT DISTINCT prd_id
+ FROM bronze.crm_cst_info
+ 
+
+ SELECT *
+ FROM bronze.erp_px_cust_g1v2
---
+++ Transformation file.sql
@@ -0,0 +1,70 @@
+
+-----CLEANING AND TRANSFORMING LOAD INTO SILVER LAYER
+
+-----crm_cst_info TABLE
+
+
+INSERT INTO silver.crm_cst_info(
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+cst_material_status,
+cst_gndr,
+cst_create_date,
+
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname)  AS cst_lastname,
+
+CASE WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
+     WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
+     ELSE 'n/a'
+END cst_material_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'n/a'
+END cst_gndr,
+
+cst_create_date
+
+FROM(
+SELECT *,
+ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cst_info
+WHERE cst_id IS NOT NULL
+)t
+WHERE flag_last =1
+
+-----crm_prd_info TABLE
+
+INSERT INTO silver.crm_prd_info(
+     prd_id,
+     cat_id,
+     prd_key,
+     prd_nm,
+     prd_cost,
+     prd_line,
+     prd_start_dt,
+     prd_end_dt
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id,
+SUBSTRING(prd_key,7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost,0) AS prd_cost,
+CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+     WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+     WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+     WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+     ELSE 'n/a'
+END prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
+
+SELECT*
+FROM silver.crm_prd_info
